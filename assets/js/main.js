76eb590839d06e4314411e48060d3ecf06ffb159
Gabriel-ggs <gabrielgomesport@gmail.com>
2023-04-11 19:58:18 -0300

    conslusao-site-fb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-ScrollReveal().reveal('.titulo-capa, .subtitulo-capa', { distance: '-50px', duration: 2000 });
-ScrollReveal().reveal('.texto-embarque', { duration: 1500 });
-
+// Scroll Navbar
 const header = document.querySelector("#container1");
 const dFixo = document.querySelector("#dFixo");
 const aFixo = document.querySelector("#aFixo");
@@ -15,10 +13,30 @@ function headerFixo(){
     a2Fixo.classList.add("afixo")
   } else if (window.scrollY==0 && header.classList.contains("fixo")){
     header.classList.remove("fixo")
-
+    header.classList.add("active")
     dFixo.classList.remove("logofixo")
     dFixo.classList.add("logo")
     aFixo.classList.remove("afixo")
     a2Fixo.classList.remove("afixo")
   }
 }
+
+// Scroll section
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + window.innerHeight*0.75;
+  target.forEach(function(element){
+    if((windowTop) > element.offsetTop){
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass)
+    }
+  })
+}
+
+animeScroll();
+window.addEventListener('scroll', function(){
+  animeScroll();
+})
